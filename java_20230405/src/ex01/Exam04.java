3d29028a0cd25ce834a47bda10596c8a5fbfb6ba
itiswhatitissss <pkmm1008@naver.com>
2023-04-05 18:02:31 +0900

    230405

--- java_20230405/src/ex01/Exam04.java
+++ java_20230405/src/ex01/Exam04.java
@@ -1,5 +1,5 @@
 package ex01;
-
+import java.util.*;
 public class Exam04 {
 
 	public static void main(String[] args) {
@@ -10,8 +10,17 @@ public class Exam04 {
 		
 		//삼항 연산자 or 조건 연산자
 
+		//ex1
 		result = (jumsu >= 60) ?  "합격" : "불합격" ; //(): 조건 , true면 합격 false면 불합격.
 		System.out.println("당신은" + result + "입니다");
+		
+		
+		//ex2 절대값
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수입력");
+		int num = sc.nextInt();
+		
+		int result = ()
 	}
 
 }
