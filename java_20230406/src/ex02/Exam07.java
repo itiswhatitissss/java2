f7583fa63d2b36817d5ba088412ae1fc03874a8c
itiswhatitissss <pkmm1008@naver.com>
2023-04-06 17:54:13 +0900

    230406

--- java_20230406/src/ex02/Exam07.java
+++ java_20230406/src/ex02/Exam07.java
@@ -14,19 +14,45 @@ public class Exam07 {
 		int c = sc.nextInt();
 		
 		System.out.println("최대값");
-		{if (a > b && a > c)
+		if (a > b && a > c)
 			System.out.println(a);
 		if (b > a && b > c)
 			System.out.println(b);
 		if (c > a && c > b)
-			System.out.println(c);}
+			System.out.println(c);
 		
 		System.out.println("최소값");
-		{if (a < b && a < c)
+		if (a < b && a < c)
 			System.out.println(a);
 		if (b < a && b < c)
 			System.out.println(b);
 		if (c < a && c < b)
-			System.out.println(c);}
-}
+			System.out.println(c);
+		
+		
+		//-----------------------------------------------//
+		int num1, num2, num3;
+		int max, min;
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		
+		//최대값
+		if(num1>num2) {
+			if(num1>num3) max = num1;
+			else max = num3;
+		}else {
+			if(num2>num3) max = num2;
+			else max = num3;
+		}
+		
+		//최소값
+		if(num1>num2) {
+			if (num2>num3) min = num3;
+			else min = num2;
+		}else {
+			if(num1>num3) min = num3;
+			else min = num1;
+		}
+	}
 }
