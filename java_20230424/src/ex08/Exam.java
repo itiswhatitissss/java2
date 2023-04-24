281dcdc4989fbd8abdca3d054111570636c191af
itiswhatitissss <pkmm1008@naver.com>
2023-04-24 17:46:57 +0900

    230424

---
+++ java_20230424/src/ex08/Exam.java
@@ -0,0 +1,26 @@
+package ex08;
+
+interface Exam {
+	void eat();
+	public abstract void sleep();
+}
+
+class cat implements Aniaml {
+	public void eat() {
+		System.out.println("고양이는 먹는다");
+	}
+	
+	public void sleep() {
+		System.out.println("고양이는 잔다");
+	}
+	
+}
+
+class dog implements Animal{
+public class Exam {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
